04afd35c352b7bbeba6e0c1c6a4b3fa51fe131d2
jaewon8405 <wo19878405@gmail.com>
2026-01-13 19:28:44 +0900

    Design: 버튼 공통 컴포넌트 ui 초기본

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import Button from "./components/Button";
+
 export default function Home() {
   return (
     <main className="p-10">
       <h1 className="typo-32-b mb-4">Taskify</h1>
-      <div className="w-[140px]">
+      <div className="w-35">
         <p className="bg-primary-tw text-white typo-32-m p-2">
           Refactoring Start (dev 브랜치에 병합)
         </p>
       </div>
+      <Button className="w-30 h-12" variant="w">
+        로그인
+      </Button>
     </main>
   );
 }
