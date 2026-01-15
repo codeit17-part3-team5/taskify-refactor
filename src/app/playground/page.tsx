ab17972eaf45ff173731f8f506d347de32dc3382
Ucastle <skaska007@naver.com>
2026-01-16 00:06:41 +0900

    feat: ColorChip 컴포넌트 생성 및 TODO 정리

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import ChipInput from "@/components/textchip/ChipInput";
+import ChipInput from '@/components/textchip/ChipInput';
+import ColorChip from '@/components/colorchip/ColorChip';
 
 export default function PlaygroundPage() {
-  const [projects, setProjects] = useState<string[]>(["프론트", "디자인"]);
+  const [projects, setProjects] = useState<string[]>(['Test']);
 
   return (
     <main>
@@ -17,13 +18,15 @@ export default function PlaygroundPage() {
           onSubmit={(e) => {
             e.preventDefault();
           }}
-          className="space-y-2 w-[360px] px-2"
+          className="space-y-2 w-[360px] px-2 mb-7"
         >
           <label htmlFor="todo-create-tags" className="typo-14-m text-black font-bold">
             태그
           </label>
           <ChipInput value={projects} onChange={setProjects} placeholder="입력 후 Enter" />
         </form>
+
+        <ColorChip />
       </section>
     </main>
   );
