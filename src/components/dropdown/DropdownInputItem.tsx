d6837825bcc8a71c7694983e026b0f22c467e23d
annmmww <kyukyu0314@naver.com>
2026-01-17 22:57:12 +0900

    feat: 선택 드롭다운 아이템 선택 시 체크 표시 렌더링

--- src/components/dropdown/DropdownInputItem.tsx
+++ src/components/dropdown/DropdownInputItem.tsx
@@ -1,18 +1,27 @@
 import type { ReactNode } from 'react';
+import Image from 'next/image';
+
+import checkItem from '@/components/dropdown/assets/check-item.svg';
 
 import { useDropdownContext } from './Dropdown';
 
 interface DropdownItemProps {
   children: ReactNode;
   height?: number;
+  isSelected: boolean;
   onSelect: () => void;
 }
 
-export const DropdownInputItem = ({ children, height = 12, onSelect }: DropdownItemProps) => {
+export const DropdownInputItem = ({
+  children,
+  height = 12,
+  isSelected,
+  onSelect,
+}: DropdownItemProps) => {
   const { handleToggle } = useDropdownContext();
 
   return (
-    <div className={`h-${height} flex items-center m-2 hover:bg-[#F1EFFD] rounded-sm`}>
+    <div className={`h-${height} flex items-center hover:bg-[#F1EFFD] rounded-sm`}>
       <button
         className="w-full text-left"
         onClick={() => {
@@ -20,7 +29,16 @@ export const DropdownInputItem = ({ children, height = 12, onSelect }: DropdownI
           handleToggle();
         }}
       >
-        <div className="mx-4 my-2 pl-7.5">{children}</div>
+        <div className="mx-4 my-2">
+          {isSelected ? (
+            <div className="flex">
+              <Image src={checkItem} alt="선택" />
+              <div className="pl-2">{children}</div>
+            </div>
+          ) : (
+            <div className="pl-7.5">{children}</div>
+          )}
+        </div>
       </button>
     </div>
   );
