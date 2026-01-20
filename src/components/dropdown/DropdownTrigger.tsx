f9e3aed0e156e1ef9c61841d4f06214f2f3bc1f6
annmmww <kyukyu0314@naver.com>
2026-01-20 19:59:06 +0900

    fix: 드롭다운 트리거 컴포넌트 병합

--- src/components/dropdown/DropdownTrigger.tsx
+++ src/components/dropdown/DropdownTrigger.tsx
@@ -1,14 +1,18 @@
 import { type ReactNode } from 'react';
+import Image from 'next/image';
 
 import { useDropdownContext } from './Dropdown';
 
 interface DropdownTriggerProps {
-  children: ReactNode;
+  label?: string | ReactNode;
   className?: string;
 }
 
-export const DropdownTrigger = ({ children, className }: DropdownTriggerProps) => {
+export const DropdownTrigger = ({ label, className }: DropdownTriggerProps) => {
   const { handleToggle } = useDropdownContext();
+
+  const BASE_CLASSES =
+    'flex flex-row border border-[#D9D9D9] focus:border-[#5534DA] rounded-md w-54 h-12';
 
   return (
     <button
@@ -16,7 +20,13 @@ export const DropdownTrigger = ({ children, className }: DropdownTriggerProps) =
         handleToggle();
       }}
     >
-      {children}
+      {' '}
+      <div className={BASE_CLASSES} tabIndex={0}>
+        <div className="w-full flex content-center justify-between items-center mx-4 my-2 ">
+          {label}
+          <Image src="/icons/arrow-dropdown.svg" alt="arrow" width={26} height={26} />
+        </div>
+      </div>
     </button>
   );
 };
